f7d75c3071ae92de0f76706222c9c36920b1a822
gabrisamartinez <41309216+gabrisamartinez@users.noreply.github.com>
2019-05-22 22:09:15 -0300

    comitar
    comitar

--- exerciciosop.java
+++ exerciciosop.java
@@ -1,24 +1,32 @@
-import java.util.ArrayList;
-import java.util.List;
-
 package com.project.sop;
 
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class Classe {
 
-	public static void main(String[] args) {
+public class Main {
+
+	public static void main(String[] args) throws Exception  {
 		while(true) {
 			List<String> listaDeString = new ArrayList<String>();
-			
-			 File file = new File("strings.txt");
+      Integer duracaoTotalCPU = null;
+      Integer duracaoTotalES = null;
+      Double tempoMedio = null;
+      Integer tempoTotalDeDuracao = null;
+
+			File file = new File("strings.txt");
+
+      Map<String, String> mapString = new HashMap<String, String>();
 			 
-			 Scanner sc = new Scanner(file);
+			Scanner sc = new Scanner(file);
 			 
 			while (sc.hasNextLine()) {
 				listaDeString.add(sc.nextLine());
 				sc.close();
-			}
-			      
+			}   
 			
 			for (String strings : listaDeString) {
 				if(strings.contains(" ")) {
@@ -32,13 +40,34 @@ public class Classe {
 						System.out.println(tipoDeProcesso);
 						System.out.println(duracao);
 						System.out.println(i);
-						
-						 Files.write(Paths.get("strings.txt"),strings.getBytes(), StandardOpenOption.APPEND);
-						
+          }
+
+          if(nomeDoProcesso.equals("CPU")) {
+              duracaoTotalCPU += duracao;
+            } else if (nomeDoProcesso.equals("ES")) {
+                duracaoTotalES += duracao;
+            }
+
+            tempoTotalDeDuracao += duracao;
+
 					}
+
+          tempoMedio = tempoTotalDeDuracao / listaDeString.size();
+            
+          mapString.add("Tempo total de execução processos CPU:", tempoTotalDeDuracao);
+
+          mapString.add("Tempo total de execução processos CPU:", duracaoTotalCPU);
+
+          mapString.add("Tempo total de execução processos ES:", duracaoTotalES);
+
+          mapString.add("Tempo de espera médio dos processos:", tempoMedio);
+
+          BufferedWriter writer = new BufferedWriter(new FileWriter("out.txt"));
+            writer.write(mapString);
+            
+            writer.close();
 				}
 			}
 		}
 	}
-
 }
