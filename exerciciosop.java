5f2c041b3229df99572e846f1a4e74cd77058097
gabrisamartinez <41309216+gabrisamartinez@users.noreply.github.com>
2019-05-22 21:10:20 -0300

    Update exerciciosop.java
    add read file and write file

--- exerciciosop.java
+++ exerciciosop.java
@@ -1,30 +1,40 @@
 import java.util.ArrayList;
 import java.util.List;
 
+package com.project.sop;
+
+
 public class Classe {
 
 	public static void main(String[] args) {
 		while(true) {
 			List<String> listaDeString = new ArrayList<String>();
 			
-			while (sc.hasNextLine()) 
-			      listaDeString.add(sc.nextLine()) ;
+			 File file = new File("strings.txt");
+			 
+			 Scanner sc = new Scanner(file);
+			 
+			while (sc.hasNextLine()) {
+				listaDeString.add(sc.nextLine());
+				sc.close();
+			}
+			      
 			
-			for (String outraCoisa : listaDeString) {
-				if(outraCoisa.contains(" ")) {
-					String[] sliptDoBagulho = outraCoisa.split(" ");
-					String nomeDoProcesso = sliptDoBagulho[0];
-					String tipoDeProcesso = sliptDoBagulho[1];
-					Integer duracao = Integer.valueOf(sliptDoBagulho[2]);
+			for (String strings : listaDeString) {
+				if(strings.contains(" ")) {
+					String[] sliptThing = strings.split(" ");
+					String nomeDoProcesso = sliptThing[0];
+					String tipoDeProcesso = sliptThing[1];
+					Integer duracao = Integer.valueOf(sliptThing[2]);
 					
 					for (int i = duracao; i >= 0; i--) {
 						System.out.println(nomeDoProcesso);
 						System.out.println(tipoDeProcesso);
 						System.out.println(duracao);
 						System.out.println(i);
-//						Integer tempoRestante = duracao; 
-//						System.out.println(tempoRestante);
-//						tempoRestante = duracao != null ? duracao - 1 : 0;
+						
+						 Files.write(Paths.get("strings.txt"),strings.getBytes(), StandardOpenOption.APPEND);
+						
 					}
 				}
 			}
